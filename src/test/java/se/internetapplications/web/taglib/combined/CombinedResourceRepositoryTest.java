6c10a73588b073e1158d4735f46f90361cacc9a1
J-M Bergqvist <jm.bergqvist@gmail.com>
2013-03-15 20:54:02 +0100

    Format file.

--- src/test/java/se/internetapplications/web/taglib/combined/CombinedResourceRepositoryTest.java
+++ src/test/java/se/internetapplications/web/taglib/combined/CombinedResourceRepositoryTest.java
@@ -1,24 +1,18 @@
 package se.internetapplications.web.taglib.combined;
 
 import static org.junit.Assert.*;
+import static se.internetapplications.web.taglib.combined.CombinedResourceRepository.*;
 
 import org.junit.Test;
-
-import se.internetapplications.web.taglib.combined.CombinedResourceRepository;
 
 public class CombinedResourceRepositoryTest {
 
     @Test
     public void testCreateScriptPathKey() {
-        assertEquals("/thirdparty", CombinedResourceRepository
-                .createResourcePathKey("", "thirdparty"));
-        assertEquals("/static/js/thirdparty", CombinedResourceRepository
-                .createResourcePathKey("/static/js", "thirdparty"));
-        assertEquals("/static/js/thirdparty", CombinedResourceRepository
-                .createResourcePathKey("/static/js", "thirdparty"));
-        assertEquals("/static/js/thirdparty", CombinedResourceRepository
-                .createResourcePathKey("static/js/", "thirdparty"));
-        assertEquals("/static/js/thirdparty", CombinedResourceRepository
-                .createResourcePathKey("/static/js/", "thirdparty"));
+        assertEquals("/thirdparty", createResourcePathKey("", "thirdparty"));
+        assertEquals("/static/js/thirdparty", createResourcePathKey("/static/js", "thirdparty"));
+        assertEquals("/static/js/thirdparty", createResourcePathKey("/static/js", "thirdparty"));
+        assertEquals("/static/js/thirdparty", createResourcePathKey("static/js/", "thirdparty"));
+        assertEquals("/static/js/thirdparty", createResourcePathKey("/static/js/", "thirdparty"));
     }
 }
