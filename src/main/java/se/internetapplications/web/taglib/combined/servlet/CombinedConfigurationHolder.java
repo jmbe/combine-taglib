fbf01bc5c7704b823991bd9c1a2a5f4c8ec7893f
J-M Bergqvist <jm.bergqvist@gmail.com>
2013-09-13 13:41:54 +0200

    Put property flag in constant.

--- src/main/java/se/internetapplications/web/taglib/combined/servlet/CombinedConfigurationHolder.java
+++ src/main/java/se/internetapplications/web/taglib/combined/servlet/CombinedConfigurationHolder.java
@@ -2,11 +2,12 @@ package se.internetapplications.web.taglib.combined.servlet;
 
 public class CombinedConfigurationHolder {
 
+    public static final String COMBINE_DEV_MODE = "combineDevMode";
     private static Boolean devMode = null;
 
     public static boolean isDevMode() {
         if (devMode == null) {
-            String combineDevMode = System.getProperties().getProperty("combineDevMode");
+            String combineDevMode = System.getProperties().getProperty(COMBINE_DEV_MODE);
             devMode = "true".equalsIgnoreCase(combineDevMode);
         }
 
