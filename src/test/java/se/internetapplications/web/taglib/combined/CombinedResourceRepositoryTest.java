e35eb72417261d28967fc81ade7b1283f8f5f643
J-M Bergqvist <jm.bergqvist@gmail.com>
2013-09-22 21:03:04 +0200

    Fix failing tests.

--- src/test/java/se/internetapplications/web/taglib/combined/CombinedResourceRepositoryTest.java
+++ src/test/java/se/internetapplications/web/taglib/combined/CombinedResourceRepositoryTest.java
@@ -1,24 +1,32 @@
 package se.internetapplications.web.taglib.combined;
 
 import static org.junit.Assert.*;
-import static se.internetapplications.web.taglib.combined.CombinedResourceRepository.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CombinedResourceRepositoryTest {
 
+    private CombinedResourceRepository repository;
+
+    @Before
+    public void setup() {
+        this.repository = CombinedResourceRepository.get();
+    }
+
     @Test
     public void javascript_resource_path_key() {
-        assertEquals("/thirdparty/js", createResourcePathKey("thirdparty", ResourceType.js));
+        assertEquals("/thirdparty/js", repository.createResourcePathKey("thirdparty", ResourceType.js));
     }
 
     @Test
     public void key_whose_name_contains_slashes() {
-        assertEquals("/static/js/thirdparty/js", createResourcePathKey("static/js/thirdparty", ResourceType.js));
+        assertEquals("/static/js/thirdparty/js",
+                repository.createResourcePathKey("static/js/thirdparty", ResourceType.js));
     }
 
     @Test
     public void css_resource_path_key() {
-        assertEquals("/thirdparty/css", createResourcePathKey("thirdparty", ResourceType.css));
+        assertEquals("/thirdparty/css", repository.createResourcePathKey("thirdparty", ResourceType.css));
     }
 }
