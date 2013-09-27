378ad6000671b5afba3a133e91b7f5aad2ace6fe
J-M Bergqvist <jm.bergqvist@gmail.com>
2013-09-28 00:52:49 +0200

    Add content types.

--- src/main/java/se/intem/web/taglib/combined/ResourceType.java
+++ src/main/java/se/intem/web/taglib/combined/ResourceType.java
@@ -1,5 +1,16 @@
 package se.intem.web.taglib.combined;
 
 public enum ResourceType {
-    css, js
+    css, js;
+
+    public String getContentType() {
+        switch (this) {
+        case css:
+            return "text/css";
+        case js:
+            return "text/javascript";
+        }
+
+        throw new RuntimeException();
+    }
 }
