89f99afefe0bd49aec38e7f658a44caa513cb571
J-M Bergqvist <jm.bergqvist@gmail.com>
2015-07-10 00:43:20 +0200

    Add method to load multiple resources of same name from classpath

--- src/main/java/se/intem/web/taglib/combined/io/ClasspathResourceLoader.java
+++ src/main/java/se/intem/web/taglib/combined/io/ClasspathResourceLoader.java
@@ -3,7 +3,11 @@ package se.intem.web.taglib.combined.io;
 import com.google.common.base.Optional;
 import com.google.common.io.Resources;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
 
 public class ClasspathResourceLoader {
 
@@ -18,6 +22,15 @@ public class ClasspathResourceLoader {
             return Optional.of(Resources.getResource(Resources.class, resourceName));
         } catch (IllegalArgumentException e) {
             return Optional.absent();
+        }
+    }
+
+    public List<URL> findResourcesInClasspath(final String resourceName) {
+        try {
+            Enumeration<URL> resources = this.getClass().getClassLoader().getResources(resourceName);
+            return Collections.list(resources);
+        } catch (IOException e) {
+            return Collections.emptyList();
         }
     }
 
